de91d3b255875c1bef25c553a747fb4a29d11516
Moonpa <puppy1856@gmail.com>
2018-07-14 15:37:14 +0800

    First push

---
+++ Dynamic-programming_Fibonacci/ImproveFibonacci.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+unsigned long long fib(int index, unsigned long long *array)
+{
+    if(array[index] != -1)
+        return array[index];
+    
+    else if(index == 0)
+    {
+        array[0] = 0;
+        return 0;
+    }
+    else if(index == 1)
+    {
+        array[1] = 1;
+        return 1;
+    }
+    else
+    {
+        unsigned long long ans = fib(index - 2, array) + fib(index - 1, array);
+        array[index] = ans;
+        return ans;
+    }
+}
+void fib()
+{
+    int index;
+    cout << "Please enter the Fibonacci\'s index(0~90):";
+    try
+    {
+    cin >> index;
+
+    if(index < 0 || index > 90)
+        throw -1;
+    //建立答案陣列
+    unsigned long long *fibArray = new unsigned long long[index];
+    //重置答案陣列
+    for (int scan = 0; scan <= index; scan++)
+        fibArray[scan] = -1;
+    /* 列印過程
+    for (int a = 0; a <= index; a++)
+        cout << fib(a, fibArray) << endl;
+    */
+    cout << "Fibonacci : " << index << " answer is " << fib(index, fibArray) << endl;
+
+    delete[] fibArray;
+    }
+    catch(int ex)
+    {
+        cout << "Index is illgal!" << endl;
+        fib();
+    }
+}
+//動態規劃版Fibonacci數列計算
+int main()
+{
+    fib();
+
+    system("pause");
+    return 0;
+}
---
+++ Tower_of_Hanoi/Tower_of_Hanoi.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+void Towers(int Disk, string Src, string Dest, string Aux)
+{
+    if (Disk == 1)
+        cout << "Move Disk " << Disk << " from " << Src << " to " << Dest << endl;
+    else
+    {
+        //先將較小的盤子移到輔助木棒
+        Towers(Disk - 1, Src, Aux, Dest);
+
+        //移最大的盤子到目的
+        cout << "Move Disk " << Disk << " from " << Src << " to " << Dest << endl;
+
+        //將較小的盤子移到目的
+        Towers(Disk - 1, Aux, Dest, Src);
+    }
+}
+
+int main()
+{
+    int Disk;
+    cout << "Please enter the disk count: ";
+    cin >> Disk;
+    Towers(Disk, "start", "end", "support");
+
+    system("pause");
+	return 0;
+}
